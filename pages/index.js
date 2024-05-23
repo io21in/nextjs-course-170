c544c4c02b3041816bf64e0457d92d56e34a8509
Hyoungwoo In <io21in@hanmail.net>
2024-05-23 17:59:26 +0900

    lecture_175

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
 function HomePage(props) {
+  const { products } = props;
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>
+          <p>{product.title}</p>
+          <p>{product.description}</p>
+        </li>
+      ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  return { 
+    props: {
+      products: [{ id: 'p1', title: 'Product 1', description: 'This is product 1'}]
+    }
+  };
 }
 
 export default HomePage;
