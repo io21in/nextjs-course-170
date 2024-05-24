15b7b8027a10b92c3fa517a8940749be22280f92
Hyoungwoo In <io21in@hanmail.net>
2024-05-24 15:27:23 +0900

    lecture_181

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import fs from 'fs/promises';
+import Link from 'next/link';
 
 function HomePage(props) {
   const { products } = props;
@@ -7,7 +8,7 @@ function HomePage(props) {
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id}>{product.title}</li>
+        <li key={product.id}><Link href={`/${product.id}`}>{product.title}</Link></li>
       ))}
     </ul>
   );
@@ -18,6 +19,14 @@ export async function getStaticProps(context) {
   const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
   const jsonData = await fs.readFile(filePath)
   const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data'
+      }
+    }
+  }
 
   if (data.products.length === 0) {
     return { notFound: true }
