eca9b1a60443ed9cf093387f03e7f37c7c9ff66a
Hyoungwoo In <io21in@hanmail.net>
2024-05-27 17:15:01 +0900

    lecture_197

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,15 +1,12 @@
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
-
-function LastSalesPage() {
-    const [sales, setSales] = useState();
+function LastSalesPage(props) {
+    const [sales, setSales] = useState(props.sales);
     // const [isLoading, setIsLoading] = useState(false);
 
     const { data, error } =useSWR(
-        'https://nextjs-course-4ba41-default-rtdb.firebaseio.com/sales.json',
-        fetcher
+        'https://nextjs-course-4ba41-default-rtdb.firebaseio.com/sales.json',(url) => fetch(url).then((res) => res.json())
     );
 
     useEffect(() => {
@@ -52,7 +49,7 @@ function LastSalesPage() {
         return <p>Fail to load.</p>;
     }
 
-    if (!data || !sales) {
+    if (!data && !sales) {
         return <p>Loading...</p>;
     }
 
@@ -65,6 +62,25 @@ function LastSalesPage() {
             ))}
         </ul>
     );
+}
+
+export async function getStaticProps() {
+    const response = await fetch(
+        'https://nextjs-course-4ba41-default-rtdb.firebaseio.com/sales.json'
+    )
+    const data = await response.json();
+        
+    const transformedSales = [];
+
+    for (const key in data) {
+        transformedSales.push({
+            id: key,
+            username: data[key].username,
+            volume: data[key].volume,
+        });
+    }
+
+    return { props: { sales: transformedSales }, revalidate: 10};
 }
 
 export default LastSalesPage;
